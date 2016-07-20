84bf7eeb9262916b42a0057bbbb33ba414d617e9
CoffeeTimeCode <coffeetimecode@gmail.com>
2016-07-20 15:35:21 -0300

    Instanciar UIs

---
+++ Assets/InstanciarUI.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InstanciarUI : MonoBehaviour {
+
+	public GameObject _ui;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown(0))
+		{
+			Instanciar();
+		}
+	}
+
+	public void Instanciar()
+	{
+		GameObject _objeto = Instantiate (_ui,Input.mousePosition,_ui.transform.rotation) as GameObject;
+		_objeto.transform.SetParent (this.transform);
+	}
+}
